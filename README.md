a4e5515aedd431397da72c94e7d90ba4a064944f
DrMoraschi <68395930+DrMoraschi@users.noreply.github.com>
2020-10-03 18:57:02 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # SPAMBot
-A spamming bot for Minecraft
+A spamming bot for Minecraft thanks to [mineflayer](https://github.com/PrismarineJS/mineflayer). Mainly for Windows, not tested on other OS.
+
+Written in Node.js
+
+[Link](https://drmoraschi.github.io/SPAMBot/) to the page of this project.
+
+<img alt="logo" src="https://github.com/DrMoraschi/SPAMBot/raw/master/projectlogo.jpg" height="200" />
+
+## Features
+
+ * Supports 1.16.1.
+ * Infinite number of custom phrases. You can add your own phrases by editing the file `lines.txt`.
+ * Custom delay between chat messages. In milliseconds.
+ * An easy-to-use GUI with incorporated chat thanks to [mineflayer-dashboard](https://github.com/wvffle/mineflayer-dashboard).
+ * Reconnect ability in case it's kicked form the server.
+ * Online/Cracked mode support.
+
+## Install
+
+ 1. Make sure you have installed **Node** on your PC, once you have installed it, you can proceed to the next step. You can download Node [here](https://nodejs.org/).
+ 1. Create a folder somewhere in you PC.
+ 2. Extract the downloaded .zip in the folder, there should be a folder named SPAMBot-master, take the files and paste them where you want, like a folder.
+ 3. Now, open the command prompt (press WIN + R, it should open a window, type in "cmd" and hit ENTER).
+ 4. Navigate to the folder where you put the files (Example: type "cd C:\Users\DrMoraschi\Desktop\BotFolder" and hit ENTER).
+ 5. Now where are going to install **Mineflayer** and the other dependencies, type:
+	
+	`npm install`
+    
+    this will install all dependencies that are necessary.
+
+ 6. Now that all the things have been installed, the bot is ready to spam.
+ 
+## How to Use
+
+ 1. In your Command Line, repeat number 4 from "Install"; navigate to the folder where the files are located.
+ 2. There are 4 arguments that are needed, and other 2 that are optional:
+
+	"host" : This is the IP or the subdomain of the server.
+
+	"port" : This is the port of the server.
+	
+	"delay" : This is the delay between each message that is sent.
+
+	"username/gmail" : You need to type a username for the bot, write the e-mail in case the account is premium, default is "SPAMBot".
+
+	"password" : Ignore if the account is cracked, only for premium accounts.
+
+	Example:
+	
+	```node SPAM.js localhost 25565 500 mygmailcool@gmail.com mypasswordis1234```
+
+ 3. Once you've written all, hit ENTER and watch as the GUI starts and the bot connects to the server.
---
+++ SPAM.js
@@ -0,0 +1,90 @@
+var mineflayer = require('mineflayer')
+const readline = require('readline');
+const fs = require('fs');
+var spammessage
+var rownumber
+var randomnumber
+var delay = false
+var kickcount = 0
+
+if (process.argv.length < 5 || process.argv.length > 7) {
+  console.log('Usage : AFK.js \x1b[31m<host>\x1b[0m')
+  console.log('               \x1b[31m<port>\x1b[0m')
+  console.log('               \x1b[31m<delay, delay in ms between each message>\x1b[0m')
+  console.log('               \x1b[32m[<name, gmail in case the account is premium>]\x1b[0m')
+  console.log('               \x1b[32m[<password, ignore in case the account is cracked>]\x1b[0m')
+  console.log('')
+  console.log('               \x1b[31mRed\x1b[0m: Needed, \x1b[32mGreen\x1b[0m: Optional')
+  console.log('')
+  console.log('               \x1b[36mExample: node AFK.js localhost 25565 500 DrMoraschi\x1b[0m (this makes a cracked account bot')
+  console.log('                                                                   that connects to localhost and sends\x1b[0m')
+  console.log('                                                                   a message every 500 milliseconds)\x1b[0m')
+  process.exit(1)
+}
+
+startBot()
+
+const readInterface = readline.createInterface({
+  input: fs.createReadStream('lines.txt'),
+});
+
+readInterface.on('line', function(line) {
+  spammessage = line;
+});
+
+function startBot() {
+  var bot = mineflayer.createBot({
+    host: process.argv[2],
+    port: parseInt(process.argv[3]),
+    username: process.argv[5] ? process.argv[5] : 'SPAMBot',
+    password: process.argv[6]
+  })
+  
+  process.argv[5] = delay
+
+  bot.on('kicked', () =>
+    setTimeout(() => {
+      startBot()
+    }, 2000)
+  )
+
+  bot.on('spawn', () => {
+    function Start() {
+      const nthline = require('nthline'),
+          filePath = 'lines.txt',
+          rowIndex = rownumber
+      nthline(rowIndex, filePath).then(bot.chat)
+    }
+    setInterval(() => {
+      var randomnumber = Math.floor(Math.random() * (count - 0 + 1)) + 0;
+      rownumber = randomnumber
+      Start()
+    }, process.argv[4]);
+  })
+
+  bot.on('login', function() {
+    bot.loadPlugin(require('mineflayer-dashboard'))
+  })
+
+  bot.on('kicked', () => {
+    kickcount += 1
+    bot.dashboard.log('\x1b[32m<STATUS>\x1b[0m \x1b[31mI got kicked. Kick count: '+`${kickcount}\x1b[0m`)
+  })
+
+  bot.on('login', () => {
+    bot.dashboard.log('\x1b[32m<STATUS> Correctly logged in\x1b[0m')
+  })
+
+  var i;
+  var count = 0;
+  require('fs').createReadStream('lines.txt')
+  .on('data', function(chunk) {
+    for (i=0; i < chunk.length; ++i)
+      if (chunk[i] == 10) count++;
+  })
+  .on('end', function() {
+    bot.once('login', () => {
+      bot.dashboard.log('\x1b[32m<STATUS> Found '+`${count + 1} `+'phrases\x1b[0m');
+    })
+  });
+}
